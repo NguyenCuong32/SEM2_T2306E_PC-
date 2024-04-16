fffed958d66571b39de2973d5b7291379ce1de25
KhangT2305E <khangnmth2304008@fpt.edu.vn>
2024-04-16 20:27:56 +0700

    commit 1st
    commit 1st

---
+++ Question2/ProductPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Question2.Services;
+
+namespace Question2
+{
+    public partial class ProductPage : ContentPage
+    {
+        public ProductPage() 
+        { 
+            InitializeComponent();
+            BindingContext = this;
+
+            ProductService productService = new ProductService();
+            var products = productService.GetProducts();
+            ProductCollection.ItemsSource = products;
+        }
+    }
+}
