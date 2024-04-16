417daf974a81b60c277757092ae9bc58f68591f8
dung <dung@MacBook-Pro-cua-dung.local>
2024-04-16 20:51:24 +0700

    dung

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,8 +8,8 @@ internal class Program
         NormalAccount normalAccount = new NormalAccount();
         System.Console.WriteLine($"Tỷ giá USD->VNĐ là : 25000đ / 1USD");
         double dola = exchangeAccount.exchangeRate(normalAccount.checkBalancer());
-        System.Console.WriteLine($"Tài khoản của bạn VNĐ->USD là : {dola}$");
         Console.WriteLine($"Số dư trong tài khoản của bạn là : {normalAccount.checkBalancer()} đ");
+        System.Console.WriteLine($"Tài khoản của bạn VNĐ->USD là : {dola}$");
         System.Console.WriteLine("Nhập số tiền bạn muốn chuyển:");
         double sotien = Convert.ToDouble(Console.ReadLine());
         normalAccount.transfer(sotien);
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55463dc6645b009d0846846ec84239380968846c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57ca25fac4f74d8c1250b52639d4ffddcd26ecd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
