a1b885e7dc2f54a87b6b05c15bd1515c03e772d1
duongtuan113 <141711273+duongtuan113@users.noreply.github.com>
2024-04-16 20:41:56 +0700

    ”tuan”

--- ConsoleApp1/RegularAccount.cs
+++ ConsoleApp1/RegularAccount.cs
@@ -4,7 +4,7 @@ public class RegularAccount : BankAccount
 {
     public override void ChuyenKhoan(decimal SoLuong)
     {
-        SoDu -= SoLuong;
+        SoDu = SoLuong;
         Console.WriteLine($"ban da chuyen khoan {SoLuong:C} đ, So dua cua ban: {SoDu:C} đ");
     }
 }
