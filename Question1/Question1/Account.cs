3fcc32d470b1a736158a76cddf3c614c36ec06c3
duyanhpham2901 <126062002+duanh113@users.noreply.github.com>
2024-04-16 20:55:42 +0700

    11
    1

---
+++ Question1/Question1/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question1
+{
+    public class Account
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public  decimal Balancer { get; set; }
+    }
+}
