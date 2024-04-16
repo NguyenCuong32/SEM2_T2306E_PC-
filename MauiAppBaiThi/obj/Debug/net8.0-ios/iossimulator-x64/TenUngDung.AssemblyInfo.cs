6d285a07a8afa2a7d17c7c3c53308a5cff2bf0b1
hoanz1 <141747185+hoanz1@users.noreply.github.com>
2024-04-16 20:53:54 +0700

    nop

--- MauiAppBaiThi/obj/Debug/net8.0-ios/iossimulator-x64/TenUngDung.AssemblyInfo.cs
+++ MauiAppBaiThi/obj/Debug/net8.0-ios/iossimulator-x64/TenUngDung.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TenUngDung")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+31fa85d4c8c3c573b59baa7410fec1485fa6be12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+207ced23eb71aa5127c2722398548b70e6a6001e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TenUngDung")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TenUngDung")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
